25b5b111700b60e61379564d46d4e4a9217b28a8
Kinetic Nexus <86033455+KineticNexus@users.noreply.github.com>
2025-03-10 13:14:09 -0300

    Initialize analytics module

---
+++ src/analytics/__init__.py
@@ -0,0 +1,9 @@
+"""
+Analytics Module - Performance tracking and reporting
+"""
+
+from .analytics import InstagramAnalytics
+from .metrics import EngagementMetrics
+from .reporting import ReportGenerator
+
+__all__ = ['InstagramAnalytics', 'EngagementMetrics', 'ReportGenerator']
